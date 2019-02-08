ae6a417d05afa09d3c76f7ce7459708451749f83
Piotr Borissov <pboriss@a-018059.ups.u-psud.fr>
2019-02-08 17:31:43 +0100

    Pourquoi le cochon ?

--- TP2/sample_code_submission/model.py
+++ TP2/sample_code_submission/model.py
@@ -11,15 +11,16 @@ import numpy as np   # We recommend to use numpy arrays
 from os.path import isfile
 from sklearn.base import BaseEstimator
 
+
 class model(BaseEstimator):
-    ''' One Rule classifier '''
+    '''One rule Classifier'''
     def __init__(self):
         ''' The "constructor" initializes the parameters '''
         self.selected_feat = 0 	# The chosen variable/feature
         self.theta1 = 0 		# The first threshold
         self.theta2 = 0			# The second threshold
         self.is_trained=False
-
+        
     def fit(self, X, Y, F=[]):
         ''' The method "fit" trains a super-simple classifier '''
         if not F: F=[str(item) for item in range(X.shape[1])]
@@ -34,8 +35,8 @@ class model(BaseEstimator):
         mu2 = np.median(best_feat[Y==2])
         # Finally is sets two decision thresholds
         self.theta1 = (mu0+mu1)/2.
-        self.theta2 = (mu1+mu2)/2.
-        self.is_trained=True
+        self.theta2 = (mu1+mu2)/2
+        self.is_trained = True
 
     def predict(self, X):
         ''' The method "predict" classifies new test examples '''
@@ -59,3 +60,8 @@ class model(BaseEstimator):
                 self = pickle.load(f)
             print("Model reloaded from: " + modelfile)
         return self
+
+
+
+
+   
